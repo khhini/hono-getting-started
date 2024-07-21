26a974e64569a4fc8cf4ab6259957f649c1e9f18
Kiki <kiki@ebconnect.com>
2024-07-21 21:05:17 +0700

    update: Logging Format & Update Dockerfile

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,20 @@
 import * as winston from 'winston';
 
+const customJsonLogFormat = winston.format.printf(({level, message, ...rest}) => {
+  return JSON.stringify({
+    severity: level.toUpperCase(),
+    message,
+    ...rest
+  });
+}) 
+
 function setupLogger(level: string): winston.Logger {
   return winston.createLogger({
     level: level,
     format: winston.format.combine(
       winston.format.timestamp(),
-      winston.format.json()
+      customJsonLogFormat
+
     ),
     transports: [
       new winston.transports.Console({
