c4cfee7c610951b6768f71f79fe26c4d95715b52
Kiki <kiki@ebconnect.com>
2024-07-21 20:40:55 +0700

    update: add errorLevels & consoleWarnLevels in log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,10 +8,16 @@ function setupLogger(level: string): winston.Logger {
       winston.format.json()
     ),
     transports: [
-      new winston.transports.Console(),
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        consoleWarnLevels: ['warn', 'debug'],
+      }),
     ],
     rejectionHandlers: [
-      new winston.transports.Console(),
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        consoleWarnLevels: ['warn', 'debug'],
+      }),
     ]
   })
 } 
